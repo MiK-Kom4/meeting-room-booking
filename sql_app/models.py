e731d03ce738fc405049977c7d854fa75299c8f4
Mik-kom4 <androidmikiya9814@gmail.com>
2024-06-18 23:43:32 +0900

    [add]models.py

---
+++ sql_app/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False, index=True)
+
+
+class Room(Base):
+    __tablename__ = 'rooms'
+
+    room_id = Column(Integer, primary_key=True, index=True)
+    room_name = Column(String, unique=True, nullable=False, index=True)
+    capacity = Column(Integer, nullable=False)
+
+
+class Booking(Base):
+    __tablename__ = 'bookings'
+
+    booking_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.user_id', ondelete='SET NULL'), nullable=False)
+    room_id = Column(Integer, ForeignKey('rooms.room_id', ondelete='SET NULL'), nullable=False)
+    booked_num = Column(Integer, nullable=False)
+    start_time = Column(DateTime, nullable=False)
+    end_time = Column(DateTime, nullable=False)
